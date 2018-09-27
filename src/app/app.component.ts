3e7a8378fdec082325d534695cf54acf15415aa1
Henry Ohanga <ohanga.henry@gmail.com>
2018-09-27 16:58:39 +0300

    initial commit

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,10 +1,43 @@
 import { Component } from '@angular/core';
+import { Item } from './item';
+import { ITEMS } from './items';
 
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html',
-  styleUrls: ['./app.component.css']
+  styleUrls: ['./app.component.css'],
 })
 export class AppComponent {
-  title = 'shopping-list';
+  shoppingBasket: Item[] = ITEMS;
+  newItem: Item = { name: null, price: null, quantity: null };
+
+  getSubtotal(something: Item): number {
+    return something.quantity * something.price;
+  }
+
+  getTotal(): number {
+    return this.shoppingBasket.reduce((a, b) => {
+      return a + this.getSubtotal(b);
+    }, 0);
+  }
+
+  addToBasket() {
+    if (this.newItem.quantity && this.newItem.price && this.newItem.name) {
+      this.shoppingBasket.push(this.newItem);
+      
+      this.clearShoppingForm();
+    }
+  }
+
+  clearShoppingBasket() {
+    this.shoppingBasket = [];
+  }
+
+  removeItem(index: number) {
+    this.shoppingBasket.splice(index, 1);
+  }
+
+  private clearShoppingForm() {
+    this.newItem = { name: null, price: null, quantity: null };
+  }
 }
---
+++ src/app/items.ts
@@ -0,0 +1,9 @@
+import { Item } from './item';
+
+export const ITEMS: Item[] = [
+  { name: 'Soap', quantity: 1, price: 20 },
+  { name: 'Milk', quantity: 4, price: 50 },
+  { name: 'Bread', quantity: 2, price: 45 },
+  { name: 'Soda', quantity: 24, price: 30 },
+  { name: 'Pen', quantity: 10, price: 25 },
+];
